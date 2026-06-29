1a612efb8a4b73bf46163f1df8f0ada3805602a1
PsikeOmega <servercide@gmail.com>
2026-06-29 15:55:06 -0400

    Repeat startup message over serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <Arduino.h>
 #include "bsp/vision_master_e213.h"
 
-void setup() {
-    Serial.begin(115200);
-    delay(1000);
-
-    BSP::begin();
-
+void printStartupMessage() {
+    Serial.println();
     Serial.println("FT8-Heltec starting...");
     Serial.print("Board: ");
     Serial.println(BSP::boardName());
@@ -14,7 +10,25 @@ void setup() {
     Serial.println(BSP::displayName());
 }
 
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+
+    BSP::begin();
+
+    printStartupMessage();
+}
+
 void loop() {
     BSP::update();
-    delay(1000);
+
+    static unsigned long lastPrint = 0;
+    unsigned long now = millis();
+
+    if (now - lastPrint >= 6000) {
+        lastPrint = now;
+        printStartupMessage();
+    }
+
+    delay(10);
 }
