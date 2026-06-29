280a1aa25e76ad010d19a9061fe6910765c761a1
PsikeOmega <servercide@gmail.com>
2026-06-29 16:45:58 -0400

    Add e-paper display checkpoints

--- src/display/display.cpp
+++ src/display/display.cpp
@@ -13,23 +13,31 @@ void begin() {
 }
 
 void showStartup() {
+    Serial.println("Display: clear");
     display.clear();
 
+    Serial.println("Display: text 1");
     display.setCursor(0, 20);
     display.setTextSize(2);
     display.print("FT8-HELTEC");
 
+    Serial.println("Display: text 2");
     display.setCursor(0, 50);
     display.setTextSize(1);
     display.print("Vision Master E213");
 
+    Serial.println("Display: text 3");
     display.setCursor(0, 70);
     display.print("USB serial OK");
 
+    Serial.println("Display: text 4");
     display.setCursor(0, 90);
     display.print("Firmware v0.0.1");
 
+    Serial.println("Display: update");
     display.update();
+
+    Serial.println("Display: done");
 }
 
 }
