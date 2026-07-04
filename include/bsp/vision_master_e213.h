869a05b617e6cf9d2d2d921c6793546afbcae449
PsikeOmega <servercide@gmail.com>
2026-07-04 01:12:04 -0400

    fix pins after power discovery

--- include/bsp/vision_master_e213.h
+++ include/bsp/vision_master_e213.h
@@ -4,8 +4,8 @@
 
 namespace BSP {
 
-constexpr uint8_t I2C_SDA_PIN = 42;
-constexpr uint8_t I2C_SCL_PIN = 45;
+constexpr uint8_t I2C_SDA_PIN = 45;
+constexpr uint8_t I2C_SCL_PIN = 42;
 
 const char* boardName();
 const char* displayName();
